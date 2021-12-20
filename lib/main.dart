4f674d83ebee65db37481de9066d6a6a94f05924
R4zmuz <rasmus.spiegel@usa.com>
2021-12-21 00:21:17 +0200

    Firebase added

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'home_page.dart';
+import 'screens/home_page.dart';
 Future<void> main() async { 
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
